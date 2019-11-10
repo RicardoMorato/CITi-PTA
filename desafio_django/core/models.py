5d141db93787a5a603f97871062e806da4f69523
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2019-11-10 00:09:14 -0300

    Template fix

--- desafio_django/core/models.py
+++ desafio_django/core/models.py
@@ -2,18 +2,16 @@ from django.db import models
 
 # Model responsável por armazenar minhas informações pessoais.
 class Pessoal(models.Model):
-    nome = models.CharField('Nome', max_length=100)
-    # Aqui estou dando um tamanho máximo para o nome, deixei o nome deitável pq é algo editável na vida real.
 
-    idade = models.IntegerField('Idade')
+    idade = models.IntegerField('idade')
     # Deixei a idade editável pois ela muda todos os anos, não botei um máximo/mínimo pq o models não é lugar de lógica, posso fazer isso no futuro usando JS.
 
-    namora = models.BooleanField('Namoro', default=True)
+    namora = models.BooleanField('namoro', default=True)
     # Deixei esse campo como editável só pra ele não ser o diferentão, mas setei o default dele como true, pq espero que ele continue sempre assim kkkkkkk.
 
-    entrar_CITi = models.CharField("CITi", max_length=30, blank=True, null=True)
+    entrar_CITi = models.CharField("citi", max_length=75, blank=True, null=True)
     # Aqui vou botar uma mensagem sobre como está meu ânimo sobre o CITi, editável pois posso ficar mudando a mensagem co o passar do tempo.
 
 
     def __str__(self):
-        return self.nome
+        return self.idade

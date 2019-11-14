2d53afadae59a79b88e016df25a912ac8f13ba5b
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2019-11-13 23:48:03 -0300

    Debugando essa branch

--- desafio_django/core/views.py
+++ desafio_django/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
-
+from django.http import HttpResponse
+from django.core.mail import EmailMessage
 from .models import ImagemPerfil, Pessoal, Hobby, CoisasQueNaoSouBom, Entretenimento, ImagensCoisasQueGosto
 
 
@@ -19,3 +20,23 @@ class HomeView(generic.TemplateView):
         context["obras_de_entretenimento"] = Entretenimento.objects.all()
         context["imagens_coisas_que_gosto"] = ImagensCoisasQueGosto.objects.all()
         return context
+
+
+
+def index(request):
+    return render(request, 'home.html')
+
+
+def email(request):
+    nome = request.POST.get('name')
+    email = request.POST.get('email')
+    assunto = request.POST.get('subject')
+    mensagem = request.POST.get('message')
+
+
+    subject = assunto
+    body = f'Nome: {nome}\nAssunto: {assunto}\nEmail: {email}\nMensagem: {mensagem}'
+
+    mail  = EmailMessage(subject, body, 'desafioPTAdjangoCITi2019.2@gmail.com', ['desafioPTAdjangoCITi2019.2@gmail.com', 'rmr@cin.ufpe.br'])
+    result = mail.send()
+    return HttpResponse(status=201)
